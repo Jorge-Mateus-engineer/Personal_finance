ca9a09136db865ed15a1c45a51baa97590117c85
Jorge Mateus <jorgemateus1995@gmail.com>
2025-02-26 20:02:56 -0500

    Enhance account retrieval with pagination support.

--- controllers/accountController.js
+++ controllers/accountController.js
@@ -17,13 +17,13 @@ exports.getAllAccounts = async (req, res, next) => {
     //     so we only need to add the missing "$" sign to the operators
 
     // INFO_STEP:
-    // a. Actions: Filter, sorting, pagination and field selection:
+    // a. Actions: Filter, sorting, pagination, field selection and pagination:
     // Create a shallow copy of the req.query object using, we want to keep the original object for implementg the features
     //   the copy will be used to just query results
     const queryObjectCopy = {...req.query}
 
     // Remove the features parameters from the copy so they do not interfere with the query when we use Model.find()
-    const fieldsToExclude = ["sort", "pages", "fields"]
+    const fieldsToExclude = ["sort", "pages", "fields", "pageNumber", "pageSize"]
     fieldsToExclude.forEach( field => delete queryObjectCopy[field])
 
     // Turn the object into a string and use regex replacement to add the missing "$" to the operators
@@ -33,12 +33,12 @@ exports.getAllAccounts = async (req, res, next) => {
 
     // a.1 Filtering: Now we pass the modified object to Model.find() with the correct operators and
     //      save the Query returned to later chain more methods to it
-    const queryAccounts = Account.find(JSON.parse(queryObjectString));
+    let queryAccounts = Account.find(JSON.parse(queryObjectString));
 
     // a.2 Sorting: We use the original req.query object and pass the req.query.sort value to Query.sort(),
     //      using a guard clause to check if the sort parameter is in the query
     if(req.query.sort) {
-        queryAccounts.sort(req.query.sort)
+        queryAccounts = queryAccounts.sort(req.query.sort)
     }
 
     // a.3 Limiting fields: We use the original req.query object and pass the req.query.fields value to Query.sort(),
@@ -48,9 +48,26 @@ exports.getAllAccounts = async (req, res, next) => {
         // we will use the Query.select() method that accepts an array of fields to be selected, so we need to convert
         // the string "field1,field2" into ["field1", "field2"]
         const fieldsArray = req.query.fields.split(",")
-        queryAccounts.select(fieldsArray)
+        queryAccounts = queryAccounts.select(fieldsArray)
 
     }
+
+    // a.4 Pagination: to do this we expect two parameters ?pageNumber=N and pageSize=N, where N is an integer
+    // to achieve this we will we will use a combination of Query.skip() and Query.limit(), we will set default values
+    // to get the page 1 and a limit of 50 documents in case the expected parameters are not sent
+    const pageNumber = parseInt(req.query.pageNumber) || 1;
+    const pageSize = parseInt(req.query.pageSize) || 50;
+
+    console.log(pageNumber)
+    console.log(pageSize)
+
+    // To go to the specified page we need to skip an amount of documents equal to pageSize * (pageNumber - 1)
+    // so if we want to go to page 10 on pages of size 5, we have to skip 5 * (10 - 1)
+    const documentsToSkip = (pageNumber - 1) * pageSize;
+
+    queryAccounts = queryAccounts.skip(documentsToSkip).limit(pageSize);
+
+    console.log(queryAccounts.toString());
 
     queryAccounts.then((accountDocuments) => {
         if (!accountDocuments.length) {
